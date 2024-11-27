b111f317525351e5beaf1df755a714d557e2f5ae
cdtm0124 <c.mullins11@universityofgalway.ie>
2024-11-27 19:06:55 +0000

    Jenkins Commit

--- src/main/java/org/example/DemoApplication.java
+++ src/main/java/org/example/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     public String hello1() {
-        return "Hello";
+        return "Hello New Friend";
     }
 
     @RequestMapping("/hello")
